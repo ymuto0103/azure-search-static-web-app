1002521f9abaaad56d3561e4ca7adc9d3e1094f6
Dina Berry <diberry@microsoft.com>
2025-01-12 15:42:29 +0000

    Client - fix footer - calculate current year

--- client/src/components/AppFooter/AppFooter.js
+++ client/src/components/AppFooter/AppFooter.js
@@ -3,10 +3,13 @@ import React from 'react';
 import './AppFooter.css';
 
 export default function AppFooter() {
+
+  const currentYear = new Date().getFullYear();
+  
   return (
       <footer className="footer">
         <hr />
-        &copy; 2023 Microsoft
+        &copy; {currentYear} Microsoft
       </footer>
   );
 };
