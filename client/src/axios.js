463261c27ba189d8c1c429118caf1fd8a0d25be2
Dina Berry <diberry@microsoft.com>
2025-01-16 23:22:24 +0000

    Client - fix - axios response for 302

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,11 +1,14 @@
 import axios from 'axios';
 
+console.log(`process.env.REACT_APP_BACKEND_URL = ${process.env.REACT_APP_BACKEND_URL}`)
+
+// Local development: `http://localhost:7071`
+// Deployed to Azure Static Web Apps with managed functions: ``
 const instance = axios.create({
-    baseURL: 'https://jsonplaceholder.typicode.com'
+    baseURL: process.env.REACT_APP_BACKEND_URL || '',
+    validateStatus: function (status) {
+        return status >= 200 && status < 400; // Accept all status codes from 200 to 399
+      }
 });
-
-instance.defaults.headers.common['Authorization'] = 'AUTH TOKEN FROM INSTANCE';
-
-// instance.interceptors.request...
 
 export default instance;
